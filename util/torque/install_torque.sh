2d48c4ba55e105a4e3d53d824f2088a08fc900bf
Serina <serinatan@cs.toronto.edu>
2019-10-02 16:43:48 -0400

    up to date torque setup: no more sketchy torque daemon

--- util/torque/install_torque.sh
+++ util/torque/install_torque.sh
@@ -2,39 +2,35 @@
 
 if [ "$#" -lt 1 ]; then
   echo "Usage install_torque.sh num_cores"
+  exit
 fi
 
 echo "Installing torque and configure server to have $1 cores."
 
-# extract/copy files
-sudo ./torque-package-mom-linux-x86_64.sh --install
-sudo ./torque-package-server-linux-x86_64.sh --install
-sudo ldconfig
-sudo mkdir /opt/local/bin/
-sudo tar xzvf opt-local-bin.tgz -C /
-sudo cp trqauthd /opt/local/sbin
-sudo cp debian.trqauthd /etc/init.d/trqauthd
-sudo cp debian.pbs_mom /etc/init.d/pbs_mom
-sudo cp debian.pbs_sched /etc/init.d/pbs_sched
+# torque SERVER setup
+sudo apt update && sudo apt install torque-server torque-scheduler torque-client
 
-# torque configuration
+# torque SERVER setup
 hostname | sudo tee /var/spool/torque/server_name
-echo y | sudo /opt/local/sbin/pbs_server -t create
-sudo /etc/init.d/trqauthd start
+
+echo y | sudo pbs_server -t create
 
 sleep 20
 
-sudo /opt/local/bin/qmgr -c "set server scheduling=true"
-sudo /opt/local/bin/qmgr -c "create queue batch queue_type=execution"
-sudo /opt/local/bin/qmgr -c "set queue batch started=true"
-sudo /opt/local/bin/qmgr -c "set queue batch enabled=true"
-sudo /opt/local/bin/qmgr -c "set queue batch resources_default.nodes=1"
-sudo /opt/local/bin/qmgr -c "set queue batch resources_default.walltime=3600"
-sudo /opt/local/bin/qmgr -c "set server default_queue=batch"
-sudo /opt/local/bin/qterm
+sudo qmgr -c "set server scheduling=true"
+sudo qmgr -c "create queue batch queue_type=execution"
+sudo qmgr -c "set queue batch started=true"
+sudo qmgr -c "set queue batch enabled=true"
+sudo qmgr -c "set queue batch resources_default.nodes=1"
+sudo qmgr -c "set queue batch resources_default.walltime=3600"
+sudo qmgr -c "set server default_queue=batch"
 
-echo "pbs_server = 127.0.0.1" | sudo tee /var/spool/torque/mom_priv/config
-echo "localhost np=4" | sudo tee /var/spool/torque/server_priv/nodes
+sudo qterm
+
 echo "`hostname` np=$1" | sudo tee -a /var/spool/torque/server_priv/nodes
 
 
+# torque CLIENT setup
+sudo apt update && sudo apt install torque-client torque-mom
+
+echo "pbs_server = `hostname`" | sudo tee /var/spool/torque/mom_priv/config
