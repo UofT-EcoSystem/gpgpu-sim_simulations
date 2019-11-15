86f4ff3ebc46bb6bc04db7701769e51782abefb6
Serina <serinatan@cs.toronto.edu>
2019-11-15 11:49:46 -0500

    update docker run script

--- util/docker/run_docker.sh
+++ util/docker/run_docker.sh
@@ -1,2 +1,2 @@
-docker run --privileged --hostname docker.gpusim -dit --runtime=nvidia -v /scratch/serina/:/mnt --ipc=host --name gpusim gpusim_serina
+docker run --privileged --hostname <hostname> -dit --runtime=nvidia -v <project_path>:/mnt --ipc=host --name gpusim gpusim_serina
 docker exec -it gpusim bash
