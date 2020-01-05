f79ec1d3ebb1cce3b4a65bd5e409e8cbdf091aad
Serina <serinatan@cs.toronto.edu>
2020-01-05 17:17:51 -0500

    seq script handles random case

--- util/job_launching/launch/seq.py
+++ util/job_launching/launch/seq.py
@@ -8,11 +8,13 @@ from launch.constant import *
 def parse_args():
     parser = argparse.ArgumentParser("Run app in isolation mode (vanilla)")
 
-    parser.add_argument('--app', nargs='+', default=app_dict.keys, help='Apps to run')
+    parser.add_argument('--app', nargs='+', default=app_dict.keys(), help='Apps to run')
     parser.add_argument('--bench_home', default=DEFAULT_BENCH_HOME,
                         help='Benchmark home folder.')
     parser.add_argument('--no_launch', default=False, action='store_true',
                         help='Do not actually trigger job launching.')
+    parser.add_argument('--random', default=False, action='store_true',
+			help='Use random hashing for memory partition.')
 
     results = parser.parse_args()
 
@@ -21,8 +23,12 @@ def parse_args():
 
 args = parse_args()
 
-config_str = "TITANV-SEP_RW"
-jobname = 'seq'
+if args.random:
+    config_str = "TITANV-SEP_RW-RANDOM"
+    jobname = 'seq-rand'
+else:
+    config_str = "TITANV-SEP_RW"
+    jobname = 'seq'
 
 for benchmark in args.app:
     p = subprocess.run(['python3',
