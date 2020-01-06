24aa8767e76f1171ec509609850cd4e1147ed5ba
Serina <serinatan@cs.toronto.edu>
2020-01-05 22:21:38 -0500

    update grid sizes for good rodinia benchmarks

--- util/job_launching/launch/constant.py
+++ util/job_launching/launch/constant.py
@@ -1,23 +1,24 @@
 import os
-# max ctas according to [resource constraints, grid size]
-app_dict = {'cut_sgemm-0': [2, 128],
-        'cut_sgemm-1': [2, 512],
-        'cut_wmma-0': [4, 128],
-        'cut_wmma-1': [4, 1024],
-        'parb_sgemm-0': [11, 528],
-        'parb_cutcp-0': [16, 121],
-        'parb_stencil-0': [16, 1024],
-        'parb_lbm-0': [12, 18000],
-        'parb_spmv-0': [16, 1147],
-        'rod_mummer-0': [8, 5727],
-        'rod_heartwall-0': [1, 51],
-        'rod_hotspot-0': [0,0],
-        'rod_hotspot3d-0': [0,0],
-        'rod_streamcluster': [0,0],
-        'rod_pathfinder': [0,0],
-        'rod_lavamd': [0,0],
-        'rod_myocyte': [0,0],
-        }
+# max ctas according to resource constraints, grid size
+app_dict = {
+            #'cut_sgemm-0': [2, 128],
+            #'cut_sgemm-1': [2, 512],
+            #'cut_wmma-0': [4, 128],
+            #'cut_wmma-1': [4, 1024],
+            #'parb_sgemm-0': [11, 528],
+            #'parb_cutcp-0': [16, 121],
+            #'parb_stencil-0': [16, 1024],
+            #'parb_lbm-0': [12, 18000],
+            #'parb_spmv-0': [16, 1147],
+            #'rod_mummer-0': [8, 5727],
+            'rod_heartwall-0': [5, 51],
+            'rod_hotspot-0': [8, 1849],
+            'rod_hotspot3d-0': [8, 1024],
+            'rod_streamcluster-0': [0,0],
+            'rod_pathfinder-0': [8, 463],
+            'rod_lavamd-0': [0,0],
+            'rod_myocyte-0': [0,0],
+           }
 
 mem_intense = ['parb_stencil-0', 'parb_lbm-0', 'parb_spmv-0']
 
