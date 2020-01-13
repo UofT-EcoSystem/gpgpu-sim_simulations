2f3e1972472da1bb286fcab14425e342c7e7b575
Serina Tan <serinatan@cs.toronto.edu>
2020-01-13 16:28:57 -0500

    gen seq table: waves and mpki

--- util/data/scripts/gen_tables/gen_table_seq.py
+++ util/data/scripts/gen_tables/gen_table_seq.py
@@ -23,20 +23,32 @@ def parse_args():
 args = parse_args()
 
 # Read CSV file
-# df = pd.read_csv(args.csv, index_col='pair_str')
 df = pd.read_csv(args.csv)
-df.sort_values('pair_str', inplace=True)
 
 # drop any benchmarks that have zero runtime
 df = df[df['runtime'] > 0]
 
 # avg dram bandwidth
 df['avg_dram_bw'] = df['dram_bw'].transform(hi.avg_array)
+df['avg_dram_eff'] = df['dram_eff'].transform(hi.avg_array)
 
 # standard deviation of dram bandwidth among channels
 df['std_dram_bw'] = df['dram_bw'].transform(hi.std_array)
 df['ratio_dram_bw'] = df['std_dram_bw'] / df['avg_dram_bw']
 
+# MPKI
+df['MPKI'] = df['l2_total_accesses'] * df['l2_miss_rate'] / (df['instructions'] / 1000)
+
+# waves
+df['waves'] = df.apply(lambda row:
+                       row['grid_x'] * row['grid_y'] * row['grid_z']
+                       / const.num_sm_volta / const.app_dict[row['pair_str']][0], axis=1)
+
+# l2_accesses
+df['l2_access_density'] = df['l2_total_accesses'] / (df['instructions'] / 1000)
+
+# sort table based on benchmark name
+df.sort_values('pair_str', inplace=True)
+
 # Output pickle
 df.to_pickle(args.output)
-
